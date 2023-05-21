d0e1ae08c0935706a215088ce42b4cf1c33c5fb2
Mahmoud Mahajna <m.n.31@hotmail.com>
2023-05-21 16:57:37 +0300

    minor changes

--- src/main/java/com/Nawras/campaigndetails/resources/CampaignDetailsResource.java
+++ src/main/java/com/Nawras/campaigndetails/resources/CampaignDetailsResource.java
@@ -1,11 +1,7 @@
 package com.Nawras.campaigndetails.resources;
 
-
 import com.Nawras.campaigndetails.models.Campaign;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
